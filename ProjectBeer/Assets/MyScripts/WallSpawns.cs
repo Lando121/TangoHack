12dd014e0c840e2f0929eb960bb29503d8f2acf6
Rasmus Elmgren <elmgren93@gmail.com>
2017-02-26 00:02:26 +0100

    made the wallspawns only float a small distance from their spawn and then rotate (only in z axis now!)

--- ProjectBeer/Assets/MyScripts/WallSpawns.cs
+++ ProjectBeer/Assets/MyScripts/WallSpawns.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class WallSpawns : MonoBehaviour {
     public float moveSpeed = 1;
     private float randomSpeed;
+    private Vector3 startingPoint;
 
 	// Use this for initialization
 	void Start () {
         randomSpeed = Random.RandomRange(0.005f, 0.02f);
+        startingPoint = transform.position;
     }   
 
     // Update is called once per frame
     void Update () {
         transform.LookAt(Camera.main.transform);
-        float distance = (transform.position - Camera.main.transform.position).magnitude;
+        //set x and z rotation to 0.
+        float y = transform.rotation.eulerAngles.y;
+        transform.rotation.eulerAngles.Set(0, y, 0);
 
-        if ( distance > 5)
+
+        float distance = (startingPoint - transform.position).magnitude;
+
+        if ( distance < 2 + randomSpeed * 10)
         {
             Approach();
-            return;
-        } else if ( distance < 2)
-        {
-            BackUp();
             return;
         } else
         {
